0095c399fb5d27a094038f9198261d245219adf1
Tehila Alon <tehila54345@gmail.com>
2026-05-17 14:19:14 +0300

    Fix pending model changes v2

---
+++ RecipeApp.DataContext/Migrations/20260517111913_FixModelChanges2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeApp.DataContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixModelChanges2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
